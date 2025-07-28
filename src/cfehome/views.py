4090f2d6ec0ae195251ded963076b1c808b7782a
Afrehsika <afrehkris2101@gmail.com>
2025-07-28 14:59:27 +0000

    msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home(request,*args, **kwargs):
+    return about(request, *args, **kwargs)
+
+
+def about(request, *args, **kwargs):
+    qr = PageVisit.objects.filter(path=request.path)
+    qr_total =PageVisit.objects.all()
+    PageVisit.objects.create(path=request.path)
+    try: 
+        percentage = (qr.count() / qr_total.count()) * 100
+    except ZeroDivisionError:
+        percentage = 0
+    
+    context = {
+        'name': 'Kriss Ntim',
+        'page_visit': qr.count(),
+        'total_page_visits': qr_total.count(),
+        'percentage': percentage,
+    }
+    return render(request, 'home.html', context)
