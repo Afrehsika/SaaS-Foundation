238cdcd687fcc828fb7a2aa6534aaa33e4e1beb1
Afrehsika <afrehkris2101@gmail.com>
2025-07-29 08:59:27 +0000

    Adding database

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*xwwwmilzwzm+&xx-*wwcnkb^sdrpkx3)f2um#sw1wneux@y#7'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')).lower() == 'true' 
+#DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true' 
+DEBUG = config('DJANGO_DEBUG')
 
 print(f"DEBUG: {DEBUG}")
 
@@ -94,6 +96,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config('CONN_MAX_AGE',cast=int, default=30)  
+DATABASE_URL = config('DATABASE_URL',cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES ={
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age= CONN_MAX_AGE,
+            conn_health_checks= True,
+            ) 
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
